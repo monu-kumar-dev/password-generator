835ae25f96a0b960b22e984e4ce3ae1a428dd714
monu-kumar-dev <monukumar.software@gmail.com>
2026-06-23 11:15:21 +0530

    feat: add js file for the counter app

---
+++ first.js
@@ -0,0 +1,111 @@
+const bar = document.getElementById("length");
+const uppercaseCheckbox = document.getElementById("uppercase");
+const lowercaseCheckbox = document.getElementById("lowercase");
+const numberCheckbox = document.getElementById("numbers");
+const symbolCheckbox = document.getElementById("symbols");
+const passwordInput = document.getElementById("password");
+
+const lengthValue = document.getElementById("lengthValue");
+
+bar.addEventListener("input", () => {
+  lengthValue.innerText = bar.value;
+});
+
+const strength = document.getElementById("strength");
+const reason = document.getElementById("reason");
+
+function generatePassword() {
+  let allowedChars = "";
+  strength.innerText = "";
+  reason.innerText = "";
+  let score = 0;
+  const uppercasePassword = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercasePassword = "abcdefghijklmnopqrstuvwxyz";
+  const numberPassword = "0123456789";
+  const symbolPassword = "!@#$%^&*";
+
+  if (uppercaseCheckbox.checked) {
+    allowedChars += uppercasePassword;
+    score += 1;
+  }
+
+  if (lowercaseCheckbox.checked) {
+    allowedChars += lowercasePassword;
+    score += 1;
+  }
+
+  if (numberCheckbox.checked) {
+    allowedChars += numberPassword;
+    score += 1;
+  }
+
+  if (symbolCheckbox.checked) {
+    allowedChars += symbolPassword;
+    score += 1;
+  }
+
+  if (allowedChars.length === 0) {
+    alert("please select at least one option");
+    return;
+  }
+
+  // console.log(allowedChars);
+
+  let password = "";
+  const passwordLength = Number(bar.value);
+  for (let i = 0; i < passwordLength; i++) {
+    const random = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars[random];
+  }
+  // passwordInput.innerText = password;
+  passwordInput.value = password;
+
+  // checking strength
+  if (passwordLength > 10) {
+    score += 1;
+  }
+
+  if (score <= 2) {
+    strength.innerText = "weak";
+    strength.classList.remove();
+    strength.classList.add("weak");
+    reason.innerText = "Easy to crack";
+  } else if (score <= 4) {
+    strength.innerText = "medium";
+    strength.classList.remove();
+    strength.classList.add("medium");
+    reason.innerText = "Acceptable but can be improved";
+  } else {
+    strength.innerText = "strong";
+    strength.classList.remove();
+    strength.classList.add("strong");
+    reason.innerText = "Good security";
+  }
+}
+// generatePassword();
+
+const generateBtn = document.getElementById("generateBtn");
+generateBtn.addEventListener("click", generatePassword);
+
+const copyBtn = document.getElementById("copyBtn");
+
+copyBtn.addEventListener("click", async () => {
+  const password = passwordInput.value;
+
+  if (!password) {
+    alert("Generate a password first!");
+    return;
+  }
+
+  try {
+    await navigator.clipboard.writeText(password);
+
+    copyBtn.innerText = "✅ Copied";
+
+    setTimeout(() => {
+      copyBtn.innerText = "📋 Copy";
+    }, 2000);
+  } catch (error) {
+    console.log("Copy failed", error);
+  }
+});
